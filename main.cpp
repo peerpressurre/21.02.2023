93bceee511286a1eaee7b62fa8b7e2b1bf362911
peerpressurre <gsophiay@gmail.com>
2023-02-22 11:23:39 +0200

    23ff

--- main.cpp
+++ main.cpp
@@ -11,15 +11,20 @@ int* findSubset(int* A, int* B, int sizeA, int sizeB) {
     int i, j;
 
     // Iterate through A to find the first element that matches B[0]
-    for (i = 0; i <= sizeA - sizeB; i++) {
-        if (A[i] == B[0]) {
+    for (i = 0; i <= sizeA - sizeB; i++)
+    {
+        if (A[i] == B[0]) 
+        {
             // If the first element matches, check the remaining elements
-            for (j = 1; j < sizeB; j++) {
-                if (A[i + j] != B[j]) {
+            for (j = 1; j < sizeB; j++)
+            {
+                if (A[i + j] != B[j])
+                {
                     break; // Not a match, move on to next element in A
                 }
             }
-            if (j == sizeB) {
+            if (j == sizeB)
+            {
                 return &A[i]; // Found a match, return pointer to start of subset
             }
         }
@@ -27,30 +32,41 @@ int* findSubset(int* A, int* B, int sizeA, int sizeB) {
     return 0; // Subset not found, return null pointer
 }
 
+int Subset(int* arra, int* arrb, int sizea, int sizeb)
+{
+    int counter = 0;
+    int i, j, temp = 0;
+    for (size_t i = 0; i < sizeb; i++)
+    {
+        for (size_t j = temp; j < sizea; j++)
+        {
+            if (arrb[i] == arra[j])
+            {
+                counter++;
+                temp = j + 1;
+                if (counter == 2 && counter > 0)
+                {
+                    return j - 1;
+                    break;
+                }    
+                break;
+            }
+            else
+            {
+                counter = 0;
+            }
+        }
+    }
+    if (counter < 2 && counter < 0)
+    {
+        return 0;
+    }
+}
 
 int main()
 {
-    //int size;
-    //cout << "Enter size: ";
-    //cin >> size;
-    //int* A = new int[size];
-    //int* B = new int[size];
-    //int sizeA = sizeof(A) / sizeof(A[0]);
-    //int sizeB = sizeof(B) / sizeof(B[0]);
-    //for (size_t i = 0; i < size; i++)
-    //{
-    //    A[i] = rand() % 10; 
-    //    cout << A[i] << "\t";
-    //}
-    //cout << endl;
-    //for (size_t i = 0; i < size; i++)
-    //{
-    //    B[i] = rand() % 10;
-    //    cout << B[i] << "\t";
-    //}
-    //cout << endl;
     int A[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    int B[] = { 6, 7, 8 };
+    int B[] = {3, 6, 7, 8 };
     int sizeA = sizeof(A) / sizeof(A[0]);
     int sizeB = sizeof(B) / sizeof(B[0]);
     cout << "Array 1:\t";
@@ -65,13 +81,21 @@ int main()
         cout << B[i] << "\t";
     }
     cout << endl;
-    int* subset = findSubset(A, B, sizeA, sizeB);
+    int subset = Subset(A, B, sizeA, sizeB);
+    if (subset)
+    {
+        cout << "Subset found starting at index " << subset << endl;
+    }
+    else {
+        cout << "Subset not found" << endl;
+    }
+    /*int* subset = findSubset(A, B, sizeA, sizeB);
     if (subset) {
         cout << "Subset found starting at index " << subset - A << endl;
     }
     else {
         cout << "Subset not found" << endl;
-    }
+    }*/
 
     return 0;
 }
