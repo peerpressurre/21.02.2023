bb007701d42e0522628f638fdb8b6772cccc93c6
peerpressurre <gsophiay@gmail.com>
2023-02-22 12:45:57 +0200

    2

--- main.cpp
+++ main.cpp
@@ -28,15 +28,15 @@ void print_array(T* arr, int size = -1)
 }
 
 template<typename T>
-T* add_array(T size, T size_a, T size_b)
+T* no_rep(T* arr_a, T* arr_b, T size, T size_a, T size_b)
 {
     T* arr = new T[size];
-    T* arr_a = init_array<int>(size_a);
+    /*T* arr_a = init_array<int>(size_a);
     T* arr_b = init_array<int>(size_b);
     cout << "Array A:" << "\t";
     print_array<int>(arr_a);
     cout << "Array B:" << "\t";
-    print_array<int>(arr_b);
+    print_array<int>(arr_b);*/
     int counter = 0;
     for (size_t i = 0; i < size_a; i++)
     {
@@ -90,8 +90,14 @@ int main()
     int size_b;
     cin >> size_b;
     int size = size_a + size_b;
-    int* arr_3 = add_array<int>(size, size_a, size_b);
-   
+    
+    int* arr_a = init_array<int>(size_a);
+    int* arr_b = init_array<int>(size_b);
+    cout << "Array A:" << "\t";
+    print_array<int>(arr_a);
+    cout << "Array B:" << "\t";
+    print_array<int>(arr_b);
+    int* arr_3 = no_rep<int>(arr_a, arr_b, size, size_a, size_b);
     cout << "Final Array:" << "\t";
     for (size_t i = 0; i < size; i++)
     {
@@ -101,5 +107,8 @@ int main()
 
         }
     }
+    delete[] arr_a;
+    delete[] arr_b;
+    delete[] arr_3;
     return 0;
 }
